9eda32a6e9cdadddb57d6905adda843b7b1e2599
Marwan Ali <72332317+maarwanali@users.noreply.github.com>
2023-02-14 15:49:17 +0300

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -13,19 +13,3 @@ const config = {
 };
 
 module.exports = config;
-
-// const config = {
-//   user: "abcd",
-//   password: "abcd",
-//   server: "LAPTOP-AQO0KMD7",
-//   database: "chat",
-//   options: {
-//     trustServerCertificate: true,
-//     trustedConnection: false,
-//     enableArithAbort: true,
-//     instancename: "SQLEXPRESS",
-//   },
-//   port: 5555,
-// };
-
-// module.exports = config;
